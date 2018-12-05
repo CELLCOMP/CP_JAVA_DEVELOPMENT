61983f6f1b288138b041273872b3b250c1a8b7cf
octaviogamero <octaviogamerofa@gmail.com>
2018-12-05 15:40:38 -0600

    Agregacion boton imprimir

--- src/Panel_Editar.java
+++ src/Panel_Editar.java
@@ -12,6 +12,7 @@ import java.awt.Font;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
+import javax.swing.SwingConstants;
 
 public class Panel_Editar extends JPanel {
 
@@ -32,8 +33,8 @@ public class Panel_Editar extends JPanel {
 			gl_panel_opciones.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_opciones.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(panel_guardar, GroupLayout.PREFERRED_SIZE, 305, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(269, Short.MAX_VALUE))
+					.addComponent(panel_guardar, GroupLayout.PREFERRED_SIZE, 462, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
 		gl_panel_opciones.setVerticalGroup(
 			gl_panel_opciones.createParallelGroup(Alignment.LEADING)
@@ -45,42 +46,48 @@ public class Panel_Editar extends JPanel {
 		JButton btnGuardar = new JButton("Guardar");
 		btnGuardar.setIcon(new ImageIcon(Panel_Editar.class.getResource("/Icons/diskette.png")));
 		
+		
 		JButton btnGuardarComo = new JButton("Guardar Como");
 		btnGuardarComo.setIcon(new ImageIcon(Panel_Editar.class.getResource("/Icons/export-file.png")));
 		
-		JLabel lblGuardar = new JLabel("GUARDAR");
-		lblGuardar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		JLabel lblEditar = new JLabel("GUARDAR");
+		lblEditar.setFont(new Font("Tahoma", Font.BOLD, 11));
 		
 		JSeparator separator = new JSeparator();
+		
+		JButton btnImprimir = new JButton("Imprimir");
+		btnImprimir.setIcon(new ImageIcon(Panel_Editar.class.getResource("/Icons/printer.png")));
 		GroupLayout gl_panel_guardar = new GroupLayout(panel_guardar);
 		gl_panel_guardar.setHorizontalGroup(
 			gl_panel_guardar.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_guardar.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(gl_panel_guardar.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_guardar.createSequentialGroup()
+					.addGroup(gl_panel_guardar.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(separator, Alignment.LEADING)
+						.addGroup(Alignment.LEADING, gl_panel_guardar.createSequentialGroup()
 							.addComponent(btnGuardar)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(btnGuardarComo))
-						.addGroup(gl_panel_guardar.createParallelGroup(Alignment.TRAILING)
-							.addGroup(gl_panel_guardar.createSequentialGroup()
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(lblGuardar)
-								.addPreferredGap(ComponentPlacement.RELATED, 168, GroupLayout.PREFERRED_SIZE))
-							.addComponent(separator, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 227, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(19, Short.MAX_VALUE))
+							.addComponent(btnGuardarComo)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnImprimir))
+						.addGroup(Alignment.LEADING, gl_panel_guardar.createSequentialGroup()
+							.addGap(171)
+							.addComponent(lblEditar)
+							.addPreferredGap(ComponentPlacement.RELATED)))
+					.addContainerGap(87, Short.MAX_VALUE))
 		);
 		gl_panel_guardar.setVerticalGroup(
 			gl_panel_guardar.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_guardar.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblGuardar)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 7, GroupLayout.PREFERRED_SIZE)
+					.addComponent(lblEditar)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(separator, GroupLayout.PREFERRED_SIZE, 2, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addGroup(gl_panel_guardar.createParallelGroup(Alignment.BASELINE)
 						.addComponent(btnGuardar, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnGuardarComo, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnGuardarComo, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnImprimir, GroupLayout.PREFERRED_SIZE, 62, GroupLayout.PREFERRED_SIZE))
 					.addGap(56))
 		);
 		panel_guardar.setLayout(gl_panel_guardar);
